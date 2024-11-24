086ec91bad12be8859b21201acf4764b888276ec
npi3pak <ivan.s.korablin@gmail.com>
2024-11-24 22:44:56 +0200

    fix bug with out of range notes

--- code.py
+++ code.py
@@ -13,7 +13,7 @@ from volts import volts
 from logger import Logger
 
 
-logger = Logger(Logger.INFO)
+logger = Logger(Logger.DEBUG)
 
 # Setup LED
 led = digitalio.DigitalInOut(board.LED)
@@ -38,11 +38,13 @@ dac = adafruit_mcp4725.MCP4725(i2c, address=0x60)
 midi_notes = []
 pitches = []
 
+
 # Function to map 1V/Oct voltages to 12-bit values and append to pitches[] array
 def map_volts(n, volt, vref, bits):
     n = simpleio.map_range(volt, 0, vref, 0, bits)
     pitches.append(n)
     logger.debug(f"Mapping volts: label={n}, 1V/Oct={volt}, 12-bit value={pitches[-1]}")
+
 
 # Populate pitches[] and midi_notes[] arrays
 for v in volts:
@@ -50,32 +52,62 @@ for v in volts:
     midi_notes.append(v["midi"])
     logger.debug(f"Added note: MIDI note={v['midi']}, 1V/Oct={v['1vOct']}")
 
+
 # Function to convert MIDI to CV
 def midi_to_cv():
     msg = midi.receive()
     if msg is not None:
         logger.debug(f"MIDI message received: {msg}")
         if isinstance(msg, NoteOff):
-            dac.raw_value = 0
-            gate.value = False
+            print(active_notes)
+            # dac.raw_value = 0
             logger.info(f"Note Off: {msg.note}, DAC set to 0, gate off")
-        elif isinstance(msg, NoteOn):
-            logger.info(f"Note On: {msg.note}, Velocity: {msg.velocity}")
 
-            # Limit note range to defined notes in volts.py
-            if msg.note < 36:
-                msg.note = 36
-            if msg.note > 96:
-                msg.note = 96
+            if msg.note in active_notes:
+                if len(active_notes) > 1:
+                    active_notes.remove(msg.note)
+                    active_notes.sort()
+
+                    z = midi_notes.index(active_notes[-1])
+                    dac.raw_value = int(pitches[z])
+                    # return None
+            if len(active_notes) == 1 and msg.note in active_notes:
+                active_notes.clear()
+                gate.value = False
+
+            return None
+
+        if isinstance(msg, NoteOn):
+            logger.info(f"Note On: {msg.note}, Velocity: {msg.velocity}")
 
             # Compare incoming note number to midi_notes[]
             try:
-                z = midi_notes.index(msg.note)
+
+                midi_note_index = msg.note
+
+                if msg.note < 36:
+                    midi_note_index = 36
+                if msg.note > 96:
+                    midi_note_index = 96
+
+                z = midi_notes.index(midi_note_index)
                 dac.raw_value = int(pitches[z])
+
+                if msg.note not in active_notes:
+                    active_notes.append(msg.note)
+
                 gate.value = True
-                logger.info(f"Note {msg.note} mapped to DAC value {pitches[z]}, gate on")
+
+                logger.info(
+                    f"Note {msg.note} mapped to DAC value {pitches[z]}, gate on"
+                )
             except ValueError:
                 logger.error(f"Note {msg.note} not found in midi_notes array")
+
+            return None
+
+
+active_notes = []
 
 # Function to test MIDI messages
 def midi_test():
@@ -88,7 +120,9 @@ def midi_test():
         else:
             logger.debug(f"Unknown MIDI message: {msg}")
 
-logger.info("System ready...")
+
+logger.info("System ready...1")
+print(gate.value)
 
 while True:
     led.value = False
